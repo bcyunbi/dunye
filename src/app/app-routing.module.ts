3f4f042b4ef3d86abce301927c589fb247cd91ab
ququ <bcyunbi@gmail.com>
2020-05-30 04:06:44 +0800

    更改架構與路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    children: [{
+      path: 'index',
+      loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+    }]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
